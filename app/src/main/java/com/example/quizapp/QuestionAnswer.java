432373c6314fe9fd7029db07d9c399a4c6850def
Ben Othmen Ghaith <ghaithbenothmen8@gmail.com>
2023-01-14 14:54:11 +0100

    initial commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quizapp;
+
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which one is not framework?",
+            "Which company owns the Apple?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Angular","Laravel","Python","Vue.Js"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Python",
+            "Apple"
+    };
+
+}
+
